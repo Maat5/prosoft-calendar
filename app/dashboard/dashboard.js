ee60433df452f2ca4f3558f01dc403534c826414
Manuel Abrante <abrantemanuel@gmail.com>
2017-06-14 18:54:07 -0400

    handle dates

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -47,7 +47,56 @@ angular.module('app.dashboard', [])
       else {
         end = moment(start).endOf('month');
       }
+      var dates = getDates(start, end);
+      var weeks = makeWeeks(dates);;
+      console.log(weeks)
     }
 
+    // get days between to dates
+    function getDates(startDate, stopDate) {
+      var dates = [];
+      var currentDate = moment(startDate);
+      var stopDate = moment(stopDate);
+      while (currentDate <= stopDate) {
+        dates.push( moment(currentDate).format('YYYY-MM-DD') )
+        currentDate = moment(currentDate).add(1, 'days');
+      }
+      return dates;
+    }
+
+    // get an array of dates and divide it by weeks
+    function makeWeeks(dates){
+      var weeks = [];
+      var data = {};
+      _.forEach(dates, function(val, i){
+        var endOfWeek = moment(val).endOf('week').format('YYYY-MM-DD');
+        var day = moment(val);
+        var dayNumber = getDayNumber(val)
+
+        if(endOfWeek === day.format('YYYY-MM-DD')){
+          data[dayNumber] = {
+            day: moment(val).format('DD'),
+            date: moment(val).format('YYYY-MM-DD'),
+            isWeekend: (dayNumber == 6  || dayNumber == 0) ? true : false
+          };
+          weeks.push(data);
+          data = {};
+        }
+        else {
+          data[dayNumber] = {
+            day: moment(val).format('DD'),
+            date: moment(val).format('YYYY-MM-DD'),
+            isWeekend: (dayNumber == 6  || dayNumber == 0) ? true : false
+          };
+        }
+      });
+      return weeks;
+    }
+
+    // get number of week day
+    function getDayNumber(day){
+      var day = moment(day).format('d');
+      return day;
+    }
 
   }]);
