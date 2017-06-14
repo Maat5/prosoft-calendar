3dea16907cdd73e808b313a661cefb06df1cea5e
Manuel Abrante <abrantemanuel@gmail.com>
2017-06-14 18:34:41 -0400

    get data from form

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,5 +1,5 @@
 angular.module('app.dashboard', [])
-  .controller('DashboardCtrl', ['$scope', function($scope) {
+  .controller('DashboardCtrl', ['$scope','moment', 'lodash', function($scope, moment, lodash ) {
 
     $scope.daysName = [{
       number: 0,
@@ -25,5 +25,29 @@ angular.module('app.dashboard', [])
     }];
 
     $scope.daysForm = {};
+    $scope.daysGrid = [];
+    $scope.dateArray = [];
+    $scope.calendars = [];
+    //triggered at form submit
+    $scope.searchDays = function(data) {
+      handleDates(data.date, data.count);
+    };
+
+    // populate calendars
+    function handleDates(date, count) {
+      $scope.calendars = [];
+      var _date = moment(date);
+      var end =
+      $scope.monthName = _date.format('MMMM Y');
+      var start = moment(date).startOf('month');
+
+      if(count) {
+         end = moment(start).add(count, 'days');
+      }
+      else {
+        end = moment(start).endOf('month');
+      }
+    }
+
 
   }]);
