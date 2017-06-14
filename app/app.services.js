af457b00693c8b74718c2af33f3c1983ecdfd70a
Manuel Abrante <abrantemanuel@gmail.com>
2017-06-14 19:33:50 -0400

    create api service

--- app/app.services.js
+++ app/app.services.js
@@ -1,7 +1,30 @@
 angular.module('app.services', [])
-  .service('moment', function(){
+  .service('moment', function() {
     return window.moment;
   })
-  .service('lodash', function(){
+  .service('lodash', function() {
     return window._;
-  });
+  })
+  .service('ApiRequests', ['$http', '$q', function($http, $q) {
+    var key = '92b1b196-df5d-487e-8c4e-3bc96b03db65';
+
+    function getHolydays(country, year) {
+      var url = 'https://holidayapi.com/v1/holidays?key=' + key + '&country=' + country + '&year=' + year;
+      var defer = $q.defer();
+      var req = {
+        method: 'GET',
+        url: url,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
+      $http(req)
+        .then(function(res) { defer.resolve(res.data) })
+        .catch(function(err) { defer.reject(err.data) });
+      return defer.promise;
+    };
+
+    return {
+      getHolydays: getHolydays,
+    };
+  }]);
